be60ad39aca7eddc5ffaaaba4b8c7793179a9376
Christian Mazakas <christian.mazakas@gmail.com>
2024-09-04 16:07:00 -0700

    update cell comments on soundness requirements

--- libsafecxx/include/std2/cell.h
+++ libsafecxx/include/std2/cell.h
@@ -12,11 +12,69 @@
 namespace std2
 {
 
-// Interior mutability is only well-defined through `&UnsafeCell<T>`
-// so leaking a `&T` from anywhere in the `Cell` here is an inherent
-// soundness hole, which includes calling user-defined copy constructors
-// The library can only interact with the underlying T via compiler-generated
-// means such as trivial relocation.
+// `cell` must require Copy, and not Clone, of the underlying type
+// this is because it's trivial to create self-referential structs using a construct like:
+// https://users.rust-lang.org/t/why-does-cell-require-copy-instead-of-clone/5769/3
+//
+// use std::cell::UnsafeCell;
+//
+// // version of Cell with Clone
+// struct Cell<T> {
+//     data: UnsafeCell<T>
+// }
+//
+// impl<T: Clone> Cell<T> {
+//     fn new(x: T) -> Cell<T> {
+//         Cell { data: UnsafeCell::new(x) }
+//     }
+//
+//     fn get(&self) -> T {
+//         unsafe {(*self.data.get()).clone()}
+//     }
+//     fn set(&self, x: T) {
+//         unsafe {
+//             *self.data.get() = x;
+//         }
+//     }
+// }
+//
+//
+// // the "magic" type
+// struct BadClone<'a> {
+//     data: Vec<i32>,
+//     pointer: &'a Cell<Option<BadClone<'a>>>,
+// }
+//
+// impl<'a> Clone for BadClone<'a> {
+//     fn clone(&self) -> BadClone<'a> {
+//         let data = &self.data;
+//         println!("before: {:?}", *data);
+//
+//         // by assigning None, we run the Drop code, deallocating the Vec
+//         self.pointer.set(None);
+//
+//         // access data after it's freed
+//         println!("after: {:?}", *data);
+//
+//         BadClone { data: self.data.clone(), pointer: self.pointer }
+//     }
+// }
+//
+// fn main() {
+//     // our stack-local creates stable storage for the Option
+//     let cell = Cell::<Option<BadClone<'_>>>::new(None);
+//
+//     // BadClone is instantiated with the address of our stack-local
+//     // we then re-assign it, creating a self-referential structure
+//     // BadClone points to the Cell that contains it
+//     cell.set(Some(BadClone {
+//         data: vec![12345678],
+//         pointer: &cell,
+//     }));
+//
+//     // our invalidating action
+//     cell.get();
+// }
 
 template<class T+>
 class [[unsafe::sync(false)]] cell
