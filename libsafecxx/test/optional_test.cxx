ef475a431b043a18a09d4f7fd32038ff8b558bcd
Christian Mazakas <christian.mazakas@gmail.com>
2024-09-06 14:35:48 -0700

    update optional_test

--- libsafecxx/test/optional_test.cxx
+++ libsafecxx/test/optional_test.cxx
@@ -33,6 +33,23 @@ void optional_accessors() safe
     std2::optional<int> mx = .some(-1);
     assert_eq(mx.unwrap(), -1);
   }
+
+  {
+    std2::vector<int> xs{1, 2, 3, 4};
+    std2::optional<std2::vector<int>> mp = .some(rel xs);
+
+    std2::vector<int> ys{4, 3, 2, 1, 1, 2, 3, 4};
+    mp = .some(rel ys);
+
+    assert_eq((mp rel.unwrap()).size(), 8u);
+  }
+
+  {
+    std2::optional<std2::box<int>> mp = .some(std2::box<int>{1234});
+    mp = .some(std2::box<int>{4321});
+
+    assert_eq(*(mp rel.unwrap()), 4321);
+  }
 }
 
 int main() safe
