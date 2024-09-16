7344a9b146adba3655ae10917f523bc21bf5b7cc
Christian Mazakas <christian.mazakas@gmail.com>
2024-09-16 15:12:58 -0700

    add missing pragam once, licensing

--- libsafecxx/single-header/std2.h
+++ libsafecxx/single-header/std2.h
@@ -1,3 +1,9 @@
+// Copyright 2024 Sean Baxter
+// Copyright 2024 Christian Mazakas
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
 #feature on safety
 
 #include <cstdio>
