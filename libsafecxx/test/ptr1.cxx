8d00ed5e1e03b50e1d20b2593453847afe319095
Christian Mazakas <christian.mazakas@gmail.com>
2024-09-18 15:41:24 -0700

    add segfaulting test case to ptr1

--- libsafecxx/test/ptr1.cxx
+++ libsafecxx/test/ptr1.cxx
@@ -3,9 +3,15 @@
 template<typename T>
 void call/(where T:static)(T) { }
 
+template<class T>
+void call2/(a, where T: a)(int^/a, T) {}
+
 void func1(int^/static ref) {
   // Should work for calls with a lifetime parameter.
   call(ref);
+
+  int x = 1234;
+  call2(^x, ref);
 }
 
 void func2(int x) {
@@ -17,4 +23,5 @@ void func2(int x) {
 int main() {
   // This had been breaking before build_sep_18_2024-1.
   void(*pf)(int) = addr call<int>;
+  // void(*pf2)(int^/static) = addr call<int^/static>;
 }
